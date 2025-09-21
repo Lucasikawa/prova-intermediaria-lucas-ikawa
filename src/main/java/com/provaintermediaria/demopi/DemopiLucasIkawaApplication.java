5f23346eda7ec281f7d6a06fa74f45ff19a324d4
Lucas ikawa <lucasbqi@al.insper.edu.br>
2025-09-20 22:08:31 -0300

    Spring Boot Inicial

---
+++ src/main/java/com/provaintermediaria/demopi/DemopiLucasIkawaApplication.java
@@ -0,0 +1,13 @@
+package com.provaintermediaria.demopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemopiLucasIkawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemopiLucasIkawaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/provaintermediaria/demopi/DemopiLucasIkawaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provaintermediaria.demopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemopiLucasIkawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
